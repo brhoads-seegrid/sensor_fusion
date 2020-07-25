1a1f4bc4b7e1aa57fc7805417d2718b776e2a729
BlaneRhoads <brhoads@seegrid.com>
2020-07-24 20:52:47 -0400

    forgot semicolon

--- src/quiz/ransac/ransac3d.cpp
+++ src/quiz/ransac/ransac3d.cpp
@@ -45,7 +45,7 @@ pcl::PointXYZI crossProduct ( pcl::PointXYZI v1, pcl::PointXYZI v2 )
     pcl::PointXYZI v3;
     v3.x = v1.y * v2.z - v1.z * v2.y;
     v3.y = v1.z * v2.x - v1.x * v2.z;
-    v3.z = v1.x * v2.y - v1.y * v2.x );
+    v3.z = v1.x * v2.y - v1.y * v2.x;
         // Default value of intensity okay.
     return v3;
 }
